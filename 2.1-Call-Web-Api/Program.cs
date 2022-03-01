c4768939e817710e1e648212fddc909e10fea431
Simon Senger <76154207+Eb0nheart@users.noreply.github.com>
2022-03-01 20:21:34 +0100

    First example

---
+++ 2.1-Call-Web-Api/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Xml;
+
+var client = new HttpClient();
+
+var response = await client.GetAsync("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+
+var content = await response.Content.ReadAsStringAsync();
+
+var document = new XmlDocument();
+
+document.LoadXml(content);
+
+Console.WriteLine($"endpoint returned: {content}");
+Console.ReadLine();
