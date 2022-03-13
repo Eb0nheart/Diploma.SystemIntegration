a2adfab0af516150b451192649a5cf18d47c00fb
Simon Senger <76154207+Eb0nheart@users.noreply.github.com>
2022-03-13 21:14:15 +0100

    stuff

--- Case.CoreFunctionality/Implementations/ElectricityPriceService.cs
+++ Case.CoreFunctionality/Implementations/ElectricityPriceService.cs
@@ -1,11 +1,22 @@
-﻿namespace Case.CoreFunctionality.Implementations;
+﻿
+using Renci.SshNet;
+
+namespace Case.CoreFunctionality.Implementations;
 public class ElectricityPriceService : IElectricityPriceService
 {
     public async Task<Dictionary<TimeOnly, double>> GetNextDaysPricesAsync(CancellationToken token = default)
     {
+        using(var client = new SftpClient("inverter.westeurope.cloudapp.azure.com", 21, "studerende", "kmdp4gslmg46jhs"))
+        {
+            client.Connect();
+            var list = client.ListDirectory("");
+        }
+
         return new()
         {
             { TimeOnly.FromDateTime(DateTime.Now), 23.64 }
         };
     }
+
+
 }
