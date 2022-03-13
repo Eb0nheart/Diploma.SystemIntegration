f1b82bd2d0dcd409e1122c41a90389544443871b
Simon Senger <76154207+Eb0nheart@users.noreply.github.com>
2022-03-13 22:39:31 +0100

    Added tester project and made the solar stuff work

--- Case.CoreFunctionality/Implementations/ElectricityPriceService.cs
+++ Case.CoreFunctionality/Implementations/ElectricityPriceService.cs
@@ -1,22 +1,85 @@
-﻿
-using Renci.SshNet;
+﻿using System.Net;
 
 namespace Case.CoreFunctionality.Implementations;
 public class ElectricityPriceService : IElectricityPriceService
 {
+    private const string baseUri = "ftp://inverter.westeurope.cloudapp.azure.com";
+
     public async Task<Dictionary<TimeOnly, double>> GetNextDaysPricesAsync(CancellationToken token = default)
     {
-        using(var client = new SftpClient("inverter.westeurope.cloudapp.azure.com", 21, "studerende", "kmdp4gslmg46jhs"))
-        {
-            client.Connect();
-            var list = client.ListDirectory("");
-        }
+        var files = await GetFilesToDownload();
+        var usages = new List<double>();
+        files.ForEach(async file => usages.Add(await GetUsage($"{baseUri}/{file}")));
 
-        return new()
+        var data = new Dictionary<TimeOnly, double>();
+        var hour = 1;
+        usages.ForEach(usage =>
         {
-            { TimeOnly.FromDateTime(DateTime.Now), 23.64 }
-        };
+            data.Add(new TimeOnly(hour, 0), usage);
+            hour++;
+        });
+
+        return data;
     }
 
+    private async Task<double> GetUsage(string uri)
+    {
+        using var reader = GetReader(uri, WebRequestMethods.Ftp.DownloadFile);
+        var lineNumber = 1;
+        var lines = new List<string>();
 
+        while (!reader.EndOfStream)
+        {
+            var line = await reader.ReadLineAsync();
+
+            if (lineNumber <= 6)
+            {
+                lineNumber++;
+                continue;
+            }
+
+            if(line != "[wr_ende]")
+            {
+                lines.Add(line);
+            }
+        }
+
+        var firstLineColumns = lines.First().Split(';');
+        var lastLineColumns = lines.Last().Split(';');
+
+        var first = firstLineColumns.ElementAt(37);
+        var last = lastLineColumns.ElementAt(37);
+
+        var firstUsage = double.Parse(first);
+        var lastUsage = double.Parse(last);
+
+        return lastUsage - firstUsage;
+    }
+
+    private async Task<List<string?>> GetFilesToDownload()
+    {
+        using var reader = GetReader(baseUri, WebRequestMethods.Ftp.ListDirectory);
+        var hoursOfToday = DateTime.Now.Hour;
+        var filesToDownload = new List<string?>();
+
+        for (int i = 0; i <= hoursOfToday; i++)
+        {
+            filesToDownload.Add(await reader.ReadLineAsync());
+        }
+
+        return filesToDownload;
+    }
+
+    private StreamReader GetReader(string uri, string requestMethod)
+    {
+#pragma warning disable SYSLIB0014 // Type or member is obsolete
+        var request = WebRequest.Create(uri);
+#pragma warning restore SYSLIB0014 // Type or member is obsolete
+
+        request.Method = requestMethod;
+        request.Credentials = new NetworkCredential("studerende", "kmdp4gslmg46jhs");
+
+        var response = request.GetResponse();
+        return new StreamReader(response.GetResponseStream());
+    }
 }
---
+++ Case.Tester/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Case.CoreFunctionality;
+using Case.CoreFunctionality.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddAllCaseFunctionality();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var ftpstuff = serviceProvider.GetService<IElectricityPriceService>();
+
+var data = await ftpstuff.GetNextDaysPricesAsync();
+
+Console.WriteLine("Hello, World!");
